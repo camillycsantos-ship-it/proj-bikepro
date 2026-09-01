5aa0a3a9d8d954a699a8966aec8445904d95e299
camillycsantos <camilly.c.santos@ba.estudante.senai.br>
2026-09-01 16:03:24 -0300

    Produtos da lista com For

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -10,33 +10,5 @@ import { Product } from '../../interfaces/product';
 })
 
 export class HomePage {
-
-  urbanaBege: Product = {
-    id: 1,
-    title: 'Urban Classic',
-    price: 1800.0,
-    image: 'assets/imagens/urbanas/urbana-bege.png',
-  };
-
-  urbanaPreta: Product = {
-    id: 2,
-    title: 'Urban Black',
-    price: 3500.0,
-    image: 'assets/imagens/urbanas/urbana-preta.png',
-  };
-
-  urbanaVermelha: Product = {
-    id: 3,
-    title: 'Urban Flash',
-    price: 1500.0,
-    image: 'assets/imagens/urbanas/urbana-vermelha.png',
-  };
-
-  urbanaVinho: Product = {
-    id: 4,
-    title: 'Urban Lux',
-    price: 3200.0,
-    image: 'assets/imagens/urbanas/urbana-vinho.png',
-  };
-
+  bikesUrbanas: Product[] = bikesUrbanas
 }
---
+++ src/app/data/products.ts
@@ -0,0 +1,32 @@
+import { Product } from "../interfaces/product";
+
+export const bikesUrbanas: Product[] = [
+    {
+        id: 1,
+        title: 'Urban Classic',
+        price: 1800.0,
+        image: 'assets/imagens/urbanas/urbana-bege.png',
+    },
+
+    {
+        id: 2,
+        title: 'Urban Black',
+        price: 3500.0,
+        image: 'assets/imagens/urbanas/urbana-preta.png',
+    },
+
+    {
+        id: 3,
+        title: 'Urban Flash',
+        price: 1500.0,
+        image: 'assets/imagens/urbanas/urbana-vermelha.png',
+    },
+
+    {
+        id: 4,
+        title: 'Urban Lux',
+        price: 3200.0,
+        image: 'assets/imagens/urbanas/urbana-vinho.png',
+    }
+
+]
